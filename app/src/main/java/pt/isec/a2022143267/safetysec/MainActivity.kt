56531b289a5218dff3a2e4a3b66a632a8b82008d
Miguel Cardoso <miguel.ferreira.cardoso1711@gmail.com>
2025-12-22 12:50:58 +0000

    Initial commit

---
+++ app/src/main/java/pt/isec/a2022143267/safetysec/MainActivity.kt
@@ -0,0 +1,47 @@
+package pt.isec.a2022143267.safetysec
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pt.isec.a2022143267.safetysec.ui.theme.SafetYSecTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SafetYSecTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SafetYSecTheme {
+        Greeting("Android")
+    }
+}
